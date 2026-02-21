9ea4e51b8b67ffe0963e69788287fccd6de4deb2
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2026-02-21 16:57:26 -0500

    GOButton

--- Assets/Scripts/GOButtons.cs
+++ Assets/Scripts/GOButtons.cs
@@ -6,11 +6,13 @@ public class GOButtons : MonoBehaviour
 {
     public void BackButton()
     {
-       SceneManager.LoadScene("MainMenu");
+        SoundManagement.Instance.PlayUI(SoundManagement.Instance.ButtonClick);
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void ExitButton()
     {
+        SoundManagement.Instance.PlayUI(SoundManagement.Instance.ButtonClick);
         Debug.Log("I wanna go dude");
         Application.Quit();
     }
