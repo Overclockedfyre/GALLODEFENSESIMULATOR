a6c7d690def19be0ac1506c3ca7a67adad0d909e
JW404 <155033986+MineralSalt69365@users.noreply.github.com>
2026-02-21 19:07:56 -0500

    Fixed some small shopping problem

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class ShopItem : MonoBehaviour
 {
     public ItemPlacement placementController;
+    public MoneyManager moneyManager;
     public GameObject itemPrefab;
     public int cost = 25;
 
     public void Select()
     {
+        if (moneyManager.Money >= 200)
+        {
         SoundManagement.Instance.PlayUI(SoundManagement.Instance.BuyGallo);
         Debug.Log($"SHOP SELECT: {itemPrefab.name} cost={cost}");
         placementController.TakeItem(itemPrefab, cost);
+        }
+        SoundManagement.Instance.PlayUI(SoundManagement.Instance.DontBuyGallo);
+        
     }
 }
